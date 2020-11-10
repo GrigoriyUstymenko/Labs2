e339a812919561e5d88217fbb17ad8f7ce472392
GrigoriyUstymenko <gromgoriy@gmail.com>
2020-11-10 19:22:07 +0200

    Task 1 - 4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = array => {
+  const types = {};
+  for (const elem of array) {
+    const type = typeof elem;
+    types[type] === undefined ?
+      types[type] = 1 :
+      types[type]++;
+  }
+  return types;
+};
 
 module.exports = { countTypesInArray };
